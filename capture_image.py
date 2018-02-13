0b64ef8c8a8811b721654989fef7ee2798fb78af
西瓜玩偶 <xiGUAwanOU@users.noreply.github.com>
2018-02-13 23:32:12 +0100

    Update capture_image.py

--- capture_image.py
+++ capture_image.py
@@ -5,7 +5,7 @@ capture = cv2.VideoCapture(0)
 
 _, frame = capture.read()
 cv2.imwrite(
-  '/tmp/bonbon-%s.png' % time.strftime("%Y%m%d%H%M%S", time.localtime()),
+  "/tmp/bonbon-%s.png" % time.strftime("%Y%m%d%H%M%S", time.localtime()),
   frame
 )
 
