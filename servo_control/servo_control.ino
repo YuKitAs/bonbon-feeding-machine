8d0cc4067570a0ba1a3db5342dc75b1430b52d71
YuKitAs <xiguawanou@gmail.com>
2018-02-16 00:20:32 +0100

    Wrote the program to control servo.

---
+++ servo_control/servo_control.ino
@@ -0,0 +1,41 @@
+#include <Servo.h>
+
+const int UART_BPS = 9600;
+
+const int SERVO_PORT_NUM = 9;
+const int SERVO_NEUTRAL_POINT = 95;
+const int SERVO_MOVING_POINT = 97;
+const int SERVO_STEP_TIME_MS = 550;
+
+Servo servo;
+
+void setup() {
+  initializeServo();
+  initializeUartConnect();
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char readChar = Serial.read();
+    if (readChar == 'f') {
+      Serial.println("START");
+      servo.write(SERVO_MOVING_POINT);
+      delay(SERVO_STEP_TIME_MS);
+      servo.write(SERVO_NEUTRAL_POINT);
+      Serial.println("STOP");
+    } else {
+      Serial.println("UNKNOWN");
+    }
+  }
+}
+
+void initializeServo() {
+  servo.attach(SERVO_PORT_NUM);
+}
+
+void initializeUartConnect() {
+  Serial.begin(UART_BPS);
+  // Actively wait for connecting:
+  while (!Serial) {}
+}
+
