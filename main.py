bd7e7a37baf5a2bcaa56629b1d0df8d8d037522d
YuKitAs <moncher.merci@gmail.com>
2018-03-28 11:10:43 +0200

    Refactored telegram bot.

---
+++ main.py
@@ -0,0 +1,153 @@
+import logging
+import time
+from configparser import ConfigParser
+
+import os
+from emoji import emojize
+from telegram import ReplyKeyboardRemove
+from telegram.ext import run_async
+
+import webcam
+from telegram_bot import TelegramBot
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+
+config = ConfigParser()
+config.read("configuration.txt")
+
+WEBCAM_PATH = config["webcam"]["Path"]
+WEBCAM_DEFAULT_VIDEO_LENGTH = int(config["webcam"]["VideoLength"])
+
+TELEGRAM_BOT_TOKEN = config["telegram.bot"]["Token"]
+TELEGRAM_BOT_SEND_TIMEOUT = int(config["telegram.bot"]["SendTimeout"])
+
+
+@run_async
+def handle_start(bot, update):
+    update.message.reply_text(emojize(
+        """
+Hello Bonbon master! What do you want to do?
+1. /takephoto :camera:
+2. /recordvideo :video_camera:
+3. /feed :lollipop:
+4. /forwardservo
+5. /backwardservo
+        """.strip()), reply_markup=ReplyKeyboardRemove())
+
+
+@run_async
+def handle_take_photo(bot, update):
+    logging.getLogger("handle_take_photo").info("Taking photo")
+
+    image_path = webcam.capture_image(WEBCAM_PATH)
+
+    logging.getLogger("handle_take_photo").info("Sending photo stored at '%s'" % image_path)
+
+    bot.send_photo(chat_id=update.message.chat_id, photo=open(image_path, "rb"), timeout=TELEGRAM_BOT_SEND_TIMEOUT)
+
+    logging.getLogger("handle_take_photo").info("Photo sent")
+
+
+@run_async
+def handle_record_video(bot, update):
+    logging.getLogger("handle_record_video").info("Recording video")
+
+    update.message.reply_text("Recording video... Please wait a minute.")
+
+    video_path = webcam.capture_video(WEBCAM_PATH, WEBCAM_DEFAULT_VIDEO_LENGTH)
+
+    logging.getLogger("handle_record_video").info("Sending video stored at '%s'" % video_path)
+
+    bot.send_document(chat_id=update.message.chat_id, document=open(video_path, "rb"),
+                      timeout=TELEGRAM_BOT_SEND_TIMEOUT)
+
+    logging.getLogger("handle_record_video").info("Video sent")
+
+
+@run_async
+def handle_feed(bot, update):
+    logging.getLogger("handle_feed").info("Rotating feeding machine")
+
+    command_file_path = os.path.join('/tmp/bonbon/commands', 'feed_%s' % str(int(time.time())))
+    with open(command_file_path, 'a'):
+        pass
+
+    logging.getLogger("handle_feed").info("Command file wrote: %s" % command_file_path)
+
+
+@run_async
+def handle_feed(bot, update):
+    logging.getLogger("handle_feed").info("Rotating feeding machine")
+
+    command_file_path = os.path.join('/tmp/bonbon/commands', 'feed_%s' % str(int(time.time())))
+    with open(command_file_path, 'a'):
+        pass
+
+    logging.getLogger("handle_feed").info("Command file wrote: %s" % command_file_path)
+
+
+@run_async
+def handle_forward_servo(bot, update):
+    logging.getLogger("handle_forward_servo").info("Rotating servo forwards")
+
+    command_file_path = os.path.join('/tmp/bonbon/commands', 'forward_%s' % str(int(time.time())))
+    with open(command_file_path, 'a'):
+        pass
+
+    logging.getLogger("handle_forward_servo").info("Command file wrote: %s" % command_file_path)
+
+
+@run_async
+def handle_backward_servo(bot, update):
+    logging.getLogger("handle_backward_servo").info("Rotating servo backwards")
+
+    command_file_path = os.path.join('/tmp/bonbon/commands', 'backward_%s' % str(int(time.time())))
+    with open(command_file_path, 'a'):
+        pass
+
+    logging.getLogger("handle_backward_servo").info("Command file wrote: %s" % command_file_path)
+
+
+@run_async
+def handle_reset_servo(bot, update):
+    logging.getLogger("handle_reset_servo").info("Resetting servo")
+
+    command_file_path = os.path.join('/tmp/bonbon/commands', 'reset_%s' % str(int(time.time())))
+    with open(command_file_path, 'a'):
+        pass
+
+    logging.getLogger("handle_reset_servo").info("Command file wrote: %s" % command_file_path)
+
+
+@run_async
+def handle_default_message(bot, update):
+    update.message.reply_text("Bonbon loves you ~")
+    time.sleep(5)
+    update.message.reply_text(emojize(":yellow_heart:"))
+
+
+def handle_error(bot, update, error):
+    error_message = "An error has occurred: %s" % error
+
+    if update:
+        update.message.reply_text(error_message)
+
+    logging.getLogger("handle_error").error(error_message)
+
+
+def main():
+    TelegramBot.with_token(TELEGRAM_BOT_TOKEN) \
+        .add_command_handler("start", handle_start) \
+        .add_command_handler("takephoto", handle_take_photo) \
+        .add_command_handler("recordvideo", handle_record_video) \
+        .add_command_handler("feed", handle_feed) \
+        .add_command_handler("forwardservo", handle_forward_servo) \
+        .add_command_handler("backwardservo", handle_backward_servo) \
+        .add_command_handler("resetservo", handle_reset_servo) \
+        .add_default_message_handler(handle_default_message) \
+        .add_error_handler(handle_error) \
+        .start()
+
+
+if __name__ == "__main__":
+    main()
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,137 +1,30 @@
-from telegram.ext import Updater, ConversationHandler, CommandHandler, MessageHandler, Filters
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import logging
-import configparser
-import webcam
-import servo
-from emoji import emojize
 
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-config = configparser.ConfigParser()
-config.read('configuration.txt')
-
-TOKEN = config['telegram.bot']['Token']
-PATH = config['photo']['Path']
-
-VIDEO, SERVO = range(2)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-def start(_bot, update):
-    update.message.reply_text(emojize(
-        """
-Hello Bonbon master! What do you want to do?
-1. /takephoto :camera:
-2. /recordvideo :video_camera:
-3. /controlservo :lollipop:
-        """.strip()), reply_markup=ReplyKeyboardRemove())
+class TelegramBot:
+    def __init__(self, token):
+        self.updater = Updater(token)
+        self.dispatcher = self.updater.dispatcher
 
+    def start(self):
+        logging.getLogger("TelegramBot").info("Starting telegram bot")
+        self.updater.start_polling()
+        self.updater.idle()
 
-def take_photo(bot, update):
-    image = webcam.capture_image(PATH)
+    def add_command_handler(self, command, callback):
+        self.dispatcher.add_handler(CommandHandler(command, callback))
+        return self
 
-    bot.send_photo(chat_id=update.message.chat_id, photo=open(image, "rb"))
+    def add_error_handler(self, callback):
+        self.dispatcher.add_error_handler(callback)
+        return self
 
+    def add_default_message_handler(self, callback):
+        self.dispatcher.add_handler(MessageHandler(Filters.text, callback))
+        return self
 
-def set_video_length(_bot, update):
-    update.message.reply_text("How long should the video be? Please enter a number of seconds (MAX = 30). \n/cancel",
-                              reply_markup=ReplyKeyboardRemove())
-
-    return VIDEO
-
-
-def record_video(bot, update):
-    length = update.message.text
-    if length.isdigit() and (int(length) in range(1, 31)):
-        update.message.reply_text("Recording video... Please wait a minute.")
-        video = webcam.capture_video(PATH, int(length))
-        bot.send_document(chat_id=update.message.chat_id, document=open(video, "rb"), timeout=1000)
-    else:
-        update.message.reply_text("Invalid number. No video recorded.")
-
-    return ConversationHandler.END
-
-
-def confirm_video(_bot, update):
-    reply_keyboard = [["Yes", "No"]]
-
-    update.message.reply_text("Do you want to record the behavior of servo? \n/cancel",
-                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True,
-                                                               one_time_keyboard=True))
-
-    return SERVO
-
-
-def control_servo(bot, update):
-    if update.message.text == "Yes":
-        update.message.reply_text("Rotating servo and recording...", reply_markup=ReplyKeyboardRemove())
-        # servo.send_servo_signal()
-        video = webcam.capture_video(PATH, 5)
-        bot.send_document(chat_id=update.message.chat_id, document=open(video, "rb"), timeout=1000)
-    elif update.message.text == "No":
-        update.message.reply_text("Rotating servo...", reply_markup=ReplyKeyboardRemove())
-        # servo.send_servo_signal()
-
-    return ConversationHandler.END
-
-
-def cancel_conv(_bot, update):
-    update.message.reply_text("Action canceled.", reply_markup=ReplyKeyboardRemove())
-
-    return ConversationHandler.END
-
-
-def auto_reply(_bot, update):
-    update.message.reply_text(emojize("Bonbon loves you :yellow_heart:"))
-
-
-def error(_bot, update, error):
-    logger.warning('Update "%s" caused error "%s"', update, error)
-
-
-def main():
-    updater = Updater(TOKEN)
-
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    dispatcher.add_handler(CommandHandler("takephoto", take_photo))
-
-    video_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('recordvideo', set_video_length)],
-
-        states={
-            VIDEO: [MessageHandler(Filters.text, record_video)]
-        },
-
-        fallbacks=[CommandHandler('cancel', cancel_conv)]
-    )
-
-    dispatcher.add_handler(video_conv_handler)
-
-    servo_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('controlservo', confirm_video)],
-
-        states={
-            SERVO: [MessageHandler(Filters.text, control_servo)]
-        },
-
-        fallbacks=[CommandHandler('cancel', cancel_conv)]
-    )
-
-    dispatcher.add_handler(servo_conv_handler)
-
-    dispatcher.add_handler(MessageHandler(Filters.text, auto_reply))
-
-    dispatcher.add_error_handler(error)
-
-    updater.start_polling()
-
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+    @staticmethod
+    def with_token(token):
+        return TelegramBot(token)
